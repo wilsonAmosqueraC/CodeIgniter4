0859fd93be2ed32d36df100b2cdaee94016eedd3
wilsonAmosqueraC <wilsonalbertomosquera@gmail.com>
2024-09-16 22:04:00 -0500

    se pone return en prueba

--- app/Controllers/Prueba.php
+++ app/Controllers/Prueba.php
@@ -6,6 +6,6 @@ class Prueba extends BaseController
 {
     public function index2(): string
     {
-        echo " y entonces ya gates te queda en pañales para prueba ";
+        return view('y entonces ya gates te queda en pañales para prueba');
     }
 }
